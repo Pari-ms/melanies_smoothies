921dd545b85a37e7e895518bac87d5f36b688193
Pari-ms <urprism@gmail.com>
2024-08-23 16:59:19 +0530

    remove the stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select ( col('FRU
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe (pd_df)
-st.stop()
+
 
 ingredients_list = st.multiselect(
             'Choose up to ingredients'
@@ -35,7 +35,7 @@ if ingredients_list:
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
+
         st.subheader(fruit_choosen+ ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
